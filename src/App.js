5a1eb74ffdd874990e7afe4342a21365ddf3168a
Paul Guzman Peña <pguzmanp@ext.confuturo.cl>
2024-05-15 01:06:50 -0400

    Nav - Carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/Navbar';
+import ImageCarousel from './components/Carousel';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <NavigationBar />
+      <ImageCarousel />
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +21,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );

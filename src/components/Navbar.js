191498b61a770fa3bb134c7d10b8b3de42a9a987
Paul Guzman Peña <pguzmanp@confuturo.cl>
2024-07-12 14:09:25 -0400

    update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,29 +4,60 @@ import logoNav from '../assets/images/LOGO_PNG-17.svg';
 
 const NavigationBar = () => {
   return (
-    <Navbar expand="lg" className="navbar-custom" fixed="top">
-      <Container>
-        {/* <Navbar.Brand href="/" className="d-flex align-items-center">
-          <img
-            src={logoNav}
-            height="25rem" // Ajusta la altura de la imagen según sea necesario
-            width="auto" // Ancho automático para mantener la proporción
-            className="align-top me-3"
-            alt="Logo"
-          />
-        </Navbar.Brand> */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-          <Nav>
-            <Nav.Link href="/">Arriendo</Nav.Link>
-            <Nav.Link href="/">En Venta</Nav.Link>
-            <Nav.Link href="/">Terrenos</Nav.Link>
-            <Nav.Link href="/">¡Quiero Vender!</Nav.Link>
-            <Nav.Link href="/Contact">Contáctanos</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    // <Navbar expand="lg" className="navbar-custom" fixed="top">
+    //   <Container>
+    //      <Navbar.Brand href="/" className="d-flex align-items-center">
+    //       <img
+    //         src={logoNav}
+    //         height="25rem" // Ajusta la altura de la imagen según sea necesario
+    //         width="auto" // Ancho automático para mantener la proporción
+    //         className="align-top me-3"
+    //         alt="Logo"
+    //       />
+    //     </Navbar.Brand> 
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+    //       <Nav>
+    //         <Nav.Link href="/">Arriendo</Nav.Link>
+    //         <Nav.Link href="/">En Venta</Nav.Link>
+    //         <Nav.Link href="/">Terrenos</Nav.Link>
+    //         <Nav.Link href="/">¡Quiero Vender!</Nav.Link>
+    //         <Nav.Link href="/Contact">Contáctanos</Nav.Link>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+
+
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container className='image-container'>
+          <Navbar.Brand href="#home">
+            <img className='img-logo'
+              alt=""
+              src={logoNav}
+              width="30"
+              height="30"
+
+            />{' '}
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+              <Nav>
+                <Nav.Link href="/">Arriendo</Nav.Link>
+                <Nav.Link href="/">En Venta</Nav.Link>
+                <Nav.Link href="/">Terrenos</Nav.Link>
+                <Nav.Link href="/">¡Quiero Vender!</Nav.Link>
+                <Nav.Link href="/Contact">Contáctanos</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+
+
   );
 };
 

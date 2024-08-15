ea96dfd71af83a65ae427a47b8ad6d9922fd3ec1
Paul Guzmán <pguzman@bcoord.cl>
2024-08-14 22:32:26 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './App.css';
 import AppRoutes from './AppRoutes';
 import Footer from './components/Footer';
+import Construccion from './components/Construccion';
 
 function App() {
   return (
     <div className="App">
       <div className="content">
-        <AppRoutes />
+        <Construccion />
+        {/* <AppRoutes /> */}
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Construccion.js
+++ src/components/Construccion.js
@@ -1,57 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './Construccion.css'; // Crear este archivo CSS si es necesario
+import { FaFacebookF, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
-
-function App() {
-  const [email, setEmail] = useState('');
-
-  const handleChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aquí puedes enviar el email a tu backend para procesarlo
-    console.log('Email enviado:', email);
-  };
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 text-center">
-          {/* <h1>BAJO CONSTRUCCIÓN</h1> */}
-          <img
-            src="https://pinguinodigital.com/wp-content/uploads/2020/08/pagina-en-construcci%C3%B3n1.jpg"
-            alt="Under construction"
-            className="img-fluid"
-          />
-          <p>
-            Nuestro sitio web está en construcción, ¡pero estamos listos para
-            comenzar! Sorpresa especial solo para nuestros suscriptores
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Enter a valid email address"
-                value={email}
-                onChange={handleChange}
-              />
+const Construccion = () => {
+    return (
+        <div className="construccion-container">
+            <div className="content">
+                <h1>Nuestro sitio web está en construcción</h1>
+                <p>¡Pero no significa que no estemos trabajando! Contactanos en redes sociales, para saber mas acerca de nuestras propiedades.</p>
+                <div className="social-icons">
+                    <a href="https://www.facebook.com/Corretajeguzman" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+                    <a href="https://www.instagram.com/corretaje_guzman/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+                    <a href="https://api.whatsapp.com/message/ZLEHJANPNYUIC1?autoload=1&app_absent=0" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
+                </div>
             </div>
-            <button type="submit" className="btn btn-primary">
-              NOTIFICARME
-            </button>
-          </form>
-          <p>
-            ¡Regístrese ahora para recibir una notificación anticipada de nuestra
-            fecha de lanzamiento!
-          </p>
-          <p className="text-muted">Imagen de Freepik</p>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
-export default App;
+export default Construccion;
